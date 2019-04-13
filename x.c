1fe23c3b4d421bb87f015caf2b3b1bf479777afd
Alejandro Hernandez <panoramic.giggle@gmail.com>
2019-04-13 11:07:52 -0400

    patch: themedcursor
    diff --git a/config.def.h b/config.def.h
    index 0e01717..3fca9c5 100644
    --- a/config.def.h
    +++ b/config.def.h
    @@ -138,11 +138,9 @@ static unsigned int cols = 80;
     static unsigned int rows = 24;
    
     /*
    - * Default colour and shape of the mouse cursor
    + * Default shape of the mouse cursor
      */
    -static unsigned int mouseshape = XC_xterm;
    -static unsigned int mousefg = 7;
    -static unsigned int mousebg = 0;
    +static char* mouseshape = "xterm";
    
     /*
      * Color used to display font attributes when fontconfig selected a font which
    diff --git a/config.mk b/config.mk
    index 0cbb002..28a7b44 100644
    --- a/config.mk
    +++ b/config.mk
    @@ -16,7 +16,7 @@ PKG_CONFIG = pkg-config
     INCS = -I$(X11INC) \
            `$(PKG_CONFIG) --cflags fontconfig` \
            `$(PKG_CONFIG) --cflags freetype2`
    -LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
    +LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXcursor \
            `$(PKG_CONFIG) --libs fontconfig` \
            `$(PKG_CONFIG) --libs freetype2`
    
    diff --git a/x.c b/x.c
    index 0422421..6897dd6 100644
    --- a/x.c
    +++ b/x.c
    @@ -14,6 +14,7 @@
     #include <X11/keysym.h>
     #include <X11/Xft/Xft.h>
     #include <X11/XKBlib.h>
    +#include <X11/Xcursor/Xcursor.h>
    
     static char *argv0;
     #include "arg.h"
    @@ -1079,21 +1080,9 @@ xinit(int cols, int rows)
     		die("XCreateIC failed. Could not obtain input method.\n");
    
     	/* white cursor, black outline */
    -	cursor = XCreateFontCursor(xw.dpy, mouseshape);
    +    cursor = XcursorLibraryLoadCursor(xw.dpy, mouseshape);
     	XDefineCursor(xw.dpy, xw.win, cursor);
    
    -	if (XParseColor(xw.dpy, xw.cmap, colorname[mousefg], &xmousefg) == 0) {
    -		xmousefg.red   = 0xffff;
    -		xmousefg.green = 0xffff;
    -		xmousefg.blue  = 0xffff;
    -	}
    -
    -	if (XParseColor(xw.dpy, xw.cmap, colorname[mousebg], &xmousebg) == 0) {
    -		xmousebg.red   = 0x0000;
    -		xmousebg.green = 0x0000;
    -		xmousebg.blue  = 0x0000;
    -	}
    -
     	XRecolorCursor(xw.dpy, cursor, &xmousefg, &xmousebg);
    
     	xw.xembed = XInternAtom(xw.dpy, "_XEMBED", False);

--- x.c
+++ x.c
@@ -14,6 +14,7 @@
 #include <X11/keysym.h>
 #include <X11/Xft/Xft.h>
 #include <X11/XKBlib.h>
+#include <X11/Xcursor/Xcursor.h>
 
 static char *argv0;
 #include "arg.h"
@@ -1079,20 +1080,8 @@ xinit(int cols, int rows)
 		die("XCreateIC failed. Could not obtain input method.\n");
 
 	/* white cursor, black outline */
-	cursor = XCreateFontCursor(xw.dpy, mouseshape);
+    cursor = XcursorLibraryLoadCursor(xw.dpy, mouseshape);
 	XDefineCursor(xw.dpy, xw.win, cursor);
-
-	if (XParseColor(xw.dpy, xw.cmap, colorname[mousefg], &xmousefg) == 0) {
-		xmousefg.red   = 0xffff;
-		xmousefg.green = 0xffff;
-		xmousefg.blue  = 0xffff;
-	}
-
-	if (XParseColor(xw.dpy, xw.cmap, colorname[mousebg], &xmousebg) == 0) {
-		xmousebg.red   = 0x0000;
-		xmousebg.green = 0x0000;
-		xmousebg.blue  = 0x0000;
-	}
 
 	XRecolorCursor(xw.dpy, cursor, &xmousefg, &xmousebg);
 
